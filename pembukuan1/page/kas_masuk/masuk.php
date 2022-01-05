c78ad886f3a4008263c91c3ca36fb23ab7b4f90c
nans1899 <anandabakti18@gmail.com>
2022-01-06 00:30:17 +0700

    delete berhasil

--- pembukuan1/page/kas_masuk/masuk.php
+++ pembukuan1/page/kas_masuk/masuk.php
@@ -39,11 +39,10 @@
 
                                         <a id="edit_data" data-toggle="modal" data-target="#edit" data-id="<?php echo $data['kode'] ?>" data-ket="<?php echo $data['keterangan'] ?>" data-tgl="<?php echo $data['tgl']; ?>" data-jml="<?php echo $data['jumlah'] ?>" class="btn btn-info"><i class="fa fa-edit"></i>Edit</a>
 
-                                        <a onclick="hapus(<?php echo $data['kode'] ?>)" class="btn btn-danger" ><i class="fa fa-trash"></i>delete</a>
+                                        <a id="hapus_data" data-toggle="modal" data-target="#hapus" data-id="<?php echo $data['kode'] ?>" data-ket="<?php echo $data['keterangan'] ?>" data-tgl="<?php echo $data['tgl']; ?>" data-jml="<?php echo $data['jumlah'] ?>" class="btn btn-danger" ><i class="fa fa-trash"></i>delete</a>
 
                                     </td>
                                 </tr>
-
                             <?php
                                 $total = $total + $data['jumlah'];
                             }
@@ -221,15 +220,88 @@
                 <!--Akir Halaman Ubah-->
 
                 <!-- Function Hapus --> 
-                <script>
-                function hapus($kode) {
-                    $querry = `DELETE FROM pembukuan WHERE kode=${kode}`;
-                    console.log($querry);
-                    return $querry;
-                    
-                   
-                }
+                <div class="panel-body">
+                    <div class="modal fade" id="hapus" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                    <h4 class="modal-title" id="myModalLabel">Apakah anda ingin menghapus data ini?</h4>
+                                </div>
+                                <div class="modal-body" id="modal_edit">
+
+                                    <form role="form" method="POST">
+                                        <div class="form-group">
+                                            <label>Kode</label>
+                                            <input class="form-control" name="kode" placeholder="Input Kode" id="kode" readonly />
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label>Keterangan</label>
+                                            <textarea class="form-control" name="ket" rows="3" id="ket" readonly></textarea>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label>Tanggal</label>
+                                            <input class="form-control" name="tgl" type="date" id="tgl" readonly />
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label>Jumlah</label>
+                                            <input class="form-control" name="jml" type="number" id="jml" readonly/>
+                                        </div>
+
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                    <input type="submit" name="hapus" value="Hapus" class="btn btn-primary">
+                                </div>
+                                </form>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <script src="assets/js/jquery-1.10.2.js"></script>
+                <script type="text/javascript">
+                    $(document).on("click", "#hapus_data", function() {
+                        var kode = $(this).data('id');
+                        var ket = $(this).data('ket');
+                        var tgl = $(this).data('tgl');
+                        var jml = $(this).data('jml');
+
+                        $("#modal_edit #kode").val(kode);
+                        $("#modal_edit #ket").val(ket);
+                        $("#modal_edit #tgl").val(tgl);
+                        $("#modal_edit #jml").val(jml);
+
+
+                    })
                 </script>
+
+                <?php
+                if (isset($_POST['hapus'])) {
+
+                    $kode = $_POST['kode'];
+                    $ket = $_POST['ket'];
+                    $tgl = $_POST['tgl'];
+                    $jml = $_POST['jml'];
+
+                    $sql = $koneksi->query("DELETE FROM pembukuan
+                    where kode = $kode");
+                    if ($sql) {
+                ?>
+                        <script type="text/javascript">
+                            alert("Hapus Data Berhasil");
+                            window.location.href = "?page=masuk";
+                        </script>
+                <?php
+                    }
+                }
+                ?>
 
             </div>
         </div>
